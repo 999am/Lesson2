b03ded68725c93bca8cc181cf86582cfb94105bc
am999 <am9999072080@gmail.com>
2023-11-03 21:48:41 +0300

    commit_1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+    }
+
+    private static void task1() {
+    }
+
+    private static void task2() {
+    }
+}
